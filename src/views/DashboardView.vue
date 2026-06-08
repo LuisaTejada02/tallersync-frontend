a11c370c4448fb559f6b189a6d83d0886e4f6f39
LuisaTejada02 <luisaeunicealvareztejada@gmail.com>
2026-06-08 01:06:28 -0600

    diseño ciudad para dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="dashboard">
-  <div class="topbar">
 
-  <div class="topbar-left">
-    <button
-      class="menu-btn"
-      @click="menuOpen = !menuOpen"
-    >
-      ☰
-    </button>
-  </div>
+<aside
+  class="sidebar">
 
-  <div class="topbar-right">
-    <h2>TallerSync</h2>
+    <div class="sidebar-header">
+  <img
+    src="/logo.png"
+    class="sidebar-logo"
+  />
+
+  <div class="sidebar-user">
+
+    <h4>{{ userName }}</h4>
+
+    <span>
+      {{ role === 'admin'
+        ? 'Administrador'
+        : 'Cliente' }}
+    </span>
+
   </div>
 
 </div>
-
-<aside
-  class="sidebar"
-  :class="{ closed: !menuOpen }"
->
-      <div>
-        <h2>TallerSync</h2>
 
         <ul class="menu">
            <li class="menu-item">
@@ -109,7 +109,6 @@
           </ul>
 
         </ul>
-      </div>
 
       <button
         class="logout-btn"
@@ -122,12 +121,14 @@
 
     <main class="content">
 
-      <h1>Bienvenido {{ userName }}</h1>
+<h1 class="title">
+  Bienvenido {{ userName }}
+</h1>
 
 <p class="welcome">
   {{ role === 'admin'
-      ? 'Administrador del sistema'
-      : 'Cliente' }}
+      ? 'Panel de Administración'
+      : 'Panel de Cliente' }}
 </p>
 
       <div class="cards">
@@ -171,7 +172,6 @@ const vehiculosOpen = ref(false)
 const citasOpen = ref(false)
 const cotizacionesOpen = ref(false)
 const ordenesOpen = ref(false)
-const menuOpen = ref(false)
 
 const logout = () => {
   localStorage.removeItem('token')
@@ -181,25 +181,52 @@ const logout = () => {
 </script>
 
 <style scoped>
+.title{
+  color:#b6ccff;
+  font-size:28px;
+  font-weight:700;
+}
+
+.welcome{
+  color:#64748B;
+  margin-bottom:30px;
+}
+
 .topbar {
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
 
-  height: 65px;
+  height: 70px;
 
-  background: #8B1E1E;
-  color: white;
+  background: #0F172A;
+
+  border-bottom: 3px solid #8B1E1E;
 
   display: flex;
   align-items: center;
+  justify-content: space-between;
 
-  padding: 0 20px;
+  padding: 0 25px;
 
   z-index: 1000;
 }
+.topbar-logo{
+  height: 100px;
+  width: auto;
+  object-fit: contain;
+}
+.topbar-right{
+  display:flex;
+  align-items:center;
+  gap:20px;
+}
 
+.user-panel{
+  margin:0;
+  text-align:right;
+}
 .menu-btn {
   background: transparent;
   border: none;
@@ -212,70 +239,139 @@ const logout = () => {
   margin-left: auto;
   font-weight: 700;
 }
+.dashboard{
+  display:flex;
+  min-height:100vh;
 
-.dashboard {
-  display: flex;
-  min-height: 100vh;
-  background: #f8fafc;
-  padding-top: 65px;
+  background-image:
+    linear-gradient(
+      rgba(15,23,42,.65),
+      rgba(15,23,42,.65)
+    ),
+    url('/tokyo-bg.png');
+
+  background-size:cover;
+  background-position:center;
 }
 
-.sidebar {
-  width: 280px;
-  background: #6e0f12;
-  color: white;
+.sidebar{
+  width:280px;
 
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  background:
+    rgba(2,6,23,.95);
 
-  padding: 25px;
+  backdrop-filter:blur(8px);
 
-  transition: all .3s ease;
+  border-right:
+    1px solid rgba(139,30,30,.4);
+
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
+
+  padding:25px;
 }
-
+.sidebar-header{
+  margin-bottom: 30px;
+}
 .sidebar.closed {
   margin-left: -330px;
+  transform: translateX(-100%);
 }
 
 .sidebar h2 {
   margin-bottom: 30px;
   text-align: center;
 }
+.sidebar-logo{
+  width: 220px;
+
+  display: block;
+
+  margin: 0 auto 25px auto;
+
+  object-fit: contain;
+}
+
+.sidebar-user{
+  text-align:center;
+  margin-bottom:25px;
+  padding-bottom:20px;
+  border-bottom:1px solid rgba(255,255,255,.15);
+}
+
+.sidebar-user h4{
+  margin:0;
+  color:white;
+}
+
+.sidebar-user span{
+  color:#CBD5E1;
+  font-size:14px;
+}
+.user-panel{
+  text-align:center;
+  margin-bottom:25px;
+}
+
+.user-panel h4{
+  margin-bottom:5px;
+}
+
+.user-panel span{
+  color:#CBD5E1;
+  font-size:14px;
+}
 
 .menu {
   list-style: none;
   padding: 0;
 }
-
 .menu-item {
-  padding: 12px;
-  margin-bottom: 8px;
-  border-radius: 8px;
+  padding: 14px 18px;
+
+  margin-bottom: 10px;
+
+  border-radius: 12px;
+
+  color: #CBD5E1;
+
   cursor: pointer;
-  transition: .2s;
+
+  transition: all .3s ease;
 }
 
 .menu-item:hover {
-  background: rgba(255,255,255,.15);
-}
 
-.submenu {
-  list-style: none;
-  margin-left: 20px;
-  margin-bottom: 15px;
-  padding-left: 10px;
-}
+  background:
+    rgba(139,30,30,.25);
 
+  color: white;
+
+  transform:
+    translateX(6px);
+
+  box-shadow:
+    0 0 10px rgba(139,30,30,.20);
+}
 .submenu li {
-  padding: 8px;
-  color: #f3f4f6;
+
+  padding: 10px;
+
+  color: #94A3B8;
+
   cursor: pointer;
+
+  transition: .3s;
 }
 
 .submenu li:hover {
+
   color: white;
+
+  padding-left: 15px;
 }
+
 
 .logout-btn {
   width: 100%;
@@ -290,13 +386,8 @@ const logout = () => {
 
 .content {
   flex: 1;
-  padding: 40px;
+  padding: 50px;
   transition: all .3s ease;
-}
-
-.welcome {
-  color: gray;
-  margin-bottom: 30px;
 }
 
 .cards {
@@ -304,27 +395,52 @@ const logout = () => {
   grid-template-columns: repeat(4, 1fr);
   gap: 20px;
 }
-
 .card {
-  background: white;
-  border-radius: 15px;
-  padding: 25px;
-  box-shadow: 0 10px 20px rgba(0,0,0,.08);
-  transition: .2s;
+  background: rgba(15,23,42,.55);
+
+  backdrop-filter: blur(12px);
+
+  border: 1px solid transparent;
+
+  border-radius: 20px;
+
+  padding: 30px;
+
+  box-shadow:
+    0 8px 20px rgba(0,0,0,.25);
+
+  transition: all .3s ease;
 }
 
 .card:hover {
-  transform: translateY(-3px);
-}
 
+  transform: translateY(-6px);
+
+  border-color: rgba(255, 0, 0, 0.61);
+
+  box-shadow:
+    0 0 5px rgba(139,30,30,.7),
+    0 0 10px rgba(139,30,30,.6),
+    0 0 20px rgba(139,30,30,.5),
+    0 0 35px rgba(139,30,30,.3);
+}
 .card h3 {
-  margin-bottom: 10px;
-}
 
+  color:#E2E8F0;
+
+  font-family:'Poppins',sans-serif;
+
+  font-weight:500;
+}
 .card p {
-  font-size: 28px;
-  font-weight: bold;
-  color: #6e0f12;
+
+  color:white;
+
+  font-family:'Poppins',sans-serif;
+
+  font-size:40px;
+
+  font-weight:700;
 }
 
 </style>
