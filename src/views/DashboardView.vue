a39c04aee572831f669439654c94ff40091ea7c5
LuisaTejada02 <luisaeunicealvareztejada@gmail.com>
2026-06-08 02:40:47 -0600

    dashboard mejorado con iconos

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -10,22 +10,11 @@
     class="sidebar-logo"
   />
 
-  <div class="sidebar-user">
-
-    <h4>{{ userName }}</h4>
-
-    <span>
-      {{ role === 'admin'
-        ? 'Administrador'
-        : 'Cliente' }}
-    </span>
-
-  </div>
-
 </div>
 
         <ul class="menu">
            <li class="menu-item">
+            <Home :size="18" />
   Inicio
 </li>
 
@@ -33,13 +22,15 @@
   class="menu-item"
   v-if="role === 'admin'"
 >
-  Usuarios
+<Users :size="18" />  
+Usuarios
 </li>
 
 <li
   class="menu-item"
   v-if="role === 'client'"
 >
+  <User :size="18" />
   Mi Perfil
 </li>
 
@@ -48,6 +39,7 @@
             @click="vehiculosOpen = !vehiculosOpen"
           >
             {{ vehiculosOpen ? '▼' : '▶' }}
+            <Car :size="18" />
             Vehículos
           </li>
           
@@ -65,6 +57,7 @@
             @click="citasOpen = !citasOpen"
           >
             {{ citasOpen ? '▼' : '▶' }}
+            <Calendar :size="18" />
             Citas
           </li>
 
@@ -81,6 +74,7 @@
             @click="cotizacionesOpen = !cotizacionesOpen"
           >
             {{ cotizacionesOpen ? '▼' : '▶' }}
+            <FileText :size="18" />
             Cotizaciones
           </li>
 
@@ -97,6 +91,7 @@
             @click="ordenesOpen = !ordenesOpen"
           >
             {{ ordenesOpen ? '▼' : '▶' }}
+            <Wrench :size="18" />
             Órdenes
           </li>
 
@@ -120,7 +115,26 @@
     </aside>
 
     <main class="content">
+<div class="content-topbar">
 
+  <div class="page-title">
+    Inicio
+  </div>
+
+  <div class="top-actions">
+
+    <div class="notification">
+       <Bell :size="20" />
+      <span class="badge">3</span>
+    </div>
+
+    <div class="mini-user">
+      {{ userName }}
+    </div>
+
+  </div>
+
+</div>
 <h1 class="title">
   Bienvenido {{ userName }}
 </h1>
@@ -130,39 +144,63 @@
       ? 'Panel de Administración'
       : 'Panel de Cliente' }}
 </p>
+<div class="cards">
 
-      <div class="cards">
+  <div class="card">
+    <div class="card-header">
+      <Car :size="28" />
+      <h3>Vehículos</h3>
+    </div>
 
-        <div class="card">
-          <h3>Vehículos</h3>
-          <p>0</p>
-        </div>
-
-        <div class="card">
-          <h3>Citas</h3>
-          <p>0</p>
-        </div>
-
-        <div class="card">
-          <h3>Cotizaciones</h3>
-          <p>0</p>
-        </div>
-
-        <div class="card">
-          <h3>Órdenes</h3>
-          <p>0</p>
-        </div>
-
-      </div>
-
-    </main>
-
+    <p>0</p>
   </div>
+
+  <div class="card">
+    <div class="card-header">
+      <Calendar :size="28" />
+      <h3>Citas</h3>
+    </div>
+
+    <p>0</p>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <FileText :size="28" />
+      <h3>Cotizaciones</h3>
+    </div>
+
+    <p>0</p>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <Wrench :size="28" />
+      <h3>Órdenes</h3>
+    </div>
+
+    <p>0</p>
+  </div>
+
+</main>
+
+</div>
+
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import {
+  Bell,
+  Home,
+  Car,
+  Calendar,
+  FileText,
+  Wrench,
+  Users,
+  User
+} from 'lucide-vue-next'
 
 const router = useRouter()
 const userName = localStorage.getItem('userName')
@@ -177,10 +215,84 @@ const logout = () => {
   localStorage.removeItem('token')
   localStorage.removeItem('role')
   router.push('/login')
+
 }
 </script>
 
 <style scoped>
+.content-topbar{
+
+  display:flex;
+
+  justify-content:space-between;
+
+  align-items:center;
+
+  margin-bottom:40px;
+}
+
+.page-title{
+
+  color:white;
+
+  font-size:26px;
+
+  font-weight:600;
+}
+
+.top-actions{
+
+  display:flex;
+
+  align-items:center;
+
+  gap:30px;
+}
+
+.notification{
+
+  position:relative;
+
+  color:white;
+
+  font-size:22px;
+
+  cursor:pointer;
+}
+
+.badge{
+
+  position:absolute;
+
+  top:-8px;
+
+  right:-10px;
+
+  background:#8B1E1E;
+
+  color:white;
+
+  width:18px;
+
+  height:18px;
+
+  border-radius:50%;
+
+  font-size:11px;
+
+  display:flex;
+
+  align-items:center;
+
+  justify-content:center;
+}
+
+.mini-user{
+
+  color:#CBD5E1;
+
+  font-size:14px;
+}
 .title{
   color:#b6ccff;
   font-size:28px;
@@ -255,7 +367,7 @@ const logout = () => {
 }
 
 .sidebar{
-  width:280px;
+  width:250px;
 
   background:
     rgba(2,6,23,.95);
@@ -291,13 +403,6 @@ const logout = () => {
   margin: 0 auto 25px auto;
 
   object-fit: contain;
-}
-
-.sidebar-user{
-  text-align:center;
-  margin-bottom:25px;
-  padding-bottom:20px;
-  border-bottom:1px solid rgba(255,255,255,.15);
 }
 
 .sidebar-user h4{
@@ -339,6 +444,10 @@ const logout = () => {
   cursor: pointer;
 
   transition: all .3s ease;
+
+   display:flex;
+  align-items:center;
+  gap:12px;
 }
 
 .menu-item:hover {
@@ -404,8 +513,9 @@ const logout = () => {
 
   border-radius: 20px;
 
-  padding: 30px;
+  padding: 25px;
 
+  min-height: 180px;
   box-shadow:
     0 8px 20px rgba(0,0,0,.25);
 
@@ -438,9 +548,23 @@ const logout = () => {
 
   font-family:'Poppins',sans-serif;
 
-  font-size:40px;
+  font-size:48px;
 
   font-weight:700;
+  
+  margin-top: 10px;
+}
+.card-header{
+
+  display:flex;
+
+  align-items:center;
+
+  gap:12px;
+
+  margin-bottom:20px;
+
+  color:#CBD5E1;
 }
 
 </style>
